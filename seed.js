8be8f659785275527519c423d876f2fa1e11a001
Zachary <zinouye@gmail.com>
2018-02-23 15:17:15 -0800

    set up environment

---
+++ seed.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/listings/:listing_id/recommendations');
+
+// let seedDB = function(data) {
+//   for (var i = 0; i < data.length; i++) {
+
+//   }
+// };
