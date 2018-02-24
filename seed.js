54d539c606a1cbc765587d2969558b0e5c3ea02d
Zachary <zinouye@gmail.com>
2018-02-23 16:17:16 -0800

    generating fake data for a given number of listings
     Please enter the commit message for your changes. Lines starting

--- seed.js
+++ seed.js
@@ -1,9 +1,34 @@
-const mongoose = require('mongoose');
+const faker = require('faker');
 
-mongoose.connect('mongodb://localhost/listings/:listing_id/recommendations');
+const genRecommendation = (amount) => {
+  const recommendations = [];
+  for (let i = 0; i < amount; i += 1) {
+    const recommendation = {
+      title: faker.address.city(),
+      description: faker.lorem.sentences(),
+      price: faker.commerce.price(),
+      photo_url: faker.image.imageUrl(),
+    };
+    recommendations.push(recommendation);
+  }
+  return recommendations;
+};
 
-// let seedDB = function(data) {
-//   for (var i = 0; i < data.length; i++) {
+const genOneData = (id) => {
+  const recommendation = {
+    listing_id: id,
+    listing_title: faker.address.city(),
+    recommendations: genRecommendation(12),
+  };
+  return recommendation;
+};
 
-//   }
-// };
+const genAllData = (count) => {
+  const aggregateData = [];
+  for (let i = 0; i < count; i += 1) {
+    aggregateData.push(genOneData(i));
+  }
+  return aggregateData;
+};
+
+exports.genAllData = genAllData;
