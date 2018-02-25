f397696dce8d05bddaf01031eb66e4c39cabc96c
Zachary <zinouye@gmail.com>
2018-02-24 17:03:58 -0800

    deleted redundant seed.js file

--- seed.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/listings/:listing_id/recommendations');
-
-// let seedDB = function(data) {
-//   for (var i = 0; i < data.length; i++) {
-
-//   }
-// };
