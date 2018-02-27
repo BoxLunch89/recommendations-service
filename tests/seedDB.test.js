e36b699e984d105f8d1f75132175b7a77c7057c4
Zachary <zinouye@gmail.com>
2018-02-26 19:21:06 -0800

    wrote first test as example

---
+++ tests/seedDB.test.js
@@ -0,0 +1,10 @@
+const React = require('react');
+const { shallow, mount, render } = require('enzyme');
+const { seedDB, find, Recommendations } = require('../database/seedDB');
+const { expect, test, describe } = require('jest');
+
+describe('Seeding database', () => {
+  test('add 1 + 2', () => {
+    expect(1).toBe(1);
+  });
+});
