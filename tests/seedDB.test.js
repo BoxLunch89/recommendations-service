47c03e839211971017c5ed06e4bb62accce0e2e0
Zachary <zinouye@gmail.com>
2018-02-28 01:45:27 -0800

    basic test passing

--- tests/seedDB.test.js
+++ tests/seedDB.test.js
@@ -1,11 +1,17 @@
 const React = require('react');
-const { shallow, mount, render } = require('enzyme');
-const { seedDB, find, Recommendations, dropDB } = require('../database/seedDB');
-const { expect, test, describe } = require('jest');
+const { seedDB, Recommendations } = require('../database/seedDB');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/recommendations');
 
 describe('Seeding database', () => {
-  
-  test('add 1 + 2', () => {
-    expect(1).toBe(1);
+  test('Check database has 200 items in Recommendations collection', () => {
+    Recommendations.find((err, recs) => {
+      if (err) {
+        console.log(err);
+      }
+      expect(recs.length).toEqual(200);
+    });
   });
 });
+
