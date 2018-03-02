2de7a88082bff9af02b6dc812d98008e2b71ade0
Zachary <zinouye@gmail.com>
2018-03-02 14:07:50 -0800

    refactored code in recommendationList and recommendationListEntry to fit linter guidelines

--- client/app/recommendationList.jsx
+++ client/app/recommendationList.jsx
@@ -4,7 +4,6 @@ import styles from './recommendationList.css';
 
 const RecommendationList = ({ recs }) => {
   const recRows = [recs.slice(0, 4), recs.slice(4, 8), recs.slice(8, 12)];
-
   return (
     <div className={styles.divTable}>
       {recRows.map(row => <RecommendationListEntry row={row} />)}
--- client/app/recommendationListEntry.jsx
+++ client/app/recommendationListEntry.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import styles from './recommendationListEntry.css';
 
 const RecommendationListEntry = ({ row }) => (
-  <div className={styles.divTableRow}>{row.map((item) => (
+  <div className={styles.divRow}>{row.map(item => (
     <div className={styles.divCell}>
-    <img class={styles.img} src={item.photo_url} alt="Recommended Places"></img>
-    <div>{item.title}</div>
-    <div>{item.description}</div>
-    <div>{item.price}</div>
+      <img className={styles.img} src={item.photo_url} alt="Recommended Places" />
+      <div>{item.title}</div>
+      <div>{item.description}</div>
+      <div>{item.price}</div>
     </div>))}
-    </div>
-    )
+  </div>
+);
 
 exports.RecommendationListEntry = RecommendationListEntry;
