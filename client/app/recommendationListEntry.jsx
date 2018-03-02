59c275bc55d0f2b41bdbe733312f539e4cd2ecb6
Zachary <zinouye@gmail.com>
2018-03-02 00:39:38 -0800

    fixed sizing on recommendation images

--- client/app/recommendationListEntry.jsx
+++ client/app/recommendationListEntry.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from './recommendationListEntry.css';
 
 const RecommendationListEntry = ({ rec }) => (
   <div className="entry">
-    <img src={rec.photo_url} alt="Recommended Places"></img>
+    <img class={styles.img} src={rec.photo_url} alt="Recommended Places"></img>
     <div>{rec.title}</div>
     <div>{rec.description}</div>
     <div>{rec.price}</div>
