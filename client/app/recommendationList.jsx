e9e43d970f4d0c6ecea85fcfba4ac8bf4d91c1c6
Zachary <zinouye@gmail.com>
2018-03-02 09:39:26 -0800

    finished front end skeleton

--- client/app/recommendationList.jsx
+++ client/app/recommendationList.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import { RecommendationListEntry } from './recommendationListEntry.jsx';
 
 const RecommendationList = ({ recs }) => {
+  const recs1 = recs.slice(0, 4);
+  const recs2 = recs.slice(4, 8);
+  const recs3 = recs.slice(8, 12);
   return (
     <div>
-      {recs.map(item => <RecommendationListEntry rec={item} />)}
+      <div>{recs1.map(item => <RecommendationListEntry rec={item} />)}</div>
+      <div>{recs2.map(item => <RecommendationListEntry rec={item} />)}</div>
+      <div>{recs3.map(item => <RecommendationListEntry rec={item} />)}</div>
     </div>);
 };
 
