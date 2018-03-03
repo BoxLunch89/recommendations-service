3ea264c7526290faadac2f42cb497786ed59e016
Zachary <zinouye@gmail.com>
2018-03-03 14:40:32 -0800

    fixed hover over adding a box-shadow to the card

--- client/app/recommendationListEntry.jsx
+++ client/app/recommendationListEntry.jsx
@@ -5,14 +5,16 @@ const RecommendationListEntry = ({ row }) => {
 
   return (
     <div className={styles.divRow}>{row.map(item => (
-      <div className={styles.divCell}>
-        <img className={styles.img} src={item.photo_url} alt="Recommended Places" />
-        <div className={styles.ribbon}>
-          <div className={styles.price}>{`from $${item.price} *`}</div>
+      <div className={styles.card}>
+        <div className={styles.divCell}>
+          <img className={styles.img} src={item.photo_url} alt="Recommended Places" />
+          <div className={styles.ribbon}>
+            <div className={styles.price}>{`from $${item.price} *`}</div>
+          </div>
+          <div className={styles.title}>{item.title}</div>
+          <div className={styles.description}>{item.description}</div><a href="google.com">... read more</a>
+          
         </div>
-        <div className={styles.title}>{item.title}</div>
-        <div className={styles.description}>{item.description}</div><a href="google.com">... read more</a>
-        
       </div>))}
     </div>);
 };
