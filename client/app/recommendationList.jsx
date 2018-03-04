c50108527938cf4c9a96be77495f170ba9fb9dea
Zachary <zinouye@gmail.com>
2018-03-03 18:15:02 -0800

    used object destructuring with css modules

--- client/app/recommendationList.jsx
+++ client/app/recommendationList.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { RecommendationListEntry } from './recommendationListEntry.jsx';
-import styles from './recommendationList.css';
+import { divTable } from './recommendationList.css';
 
 const RecommendationList = ({ recs }) => {
   const recRows = [recs.slice(0, 4), recs.slice(4, 8), recs.slice(8, 12)];
   return (
-    <div className={styles.divTable}>
+    <div className={divTable}>
       {recRows.map(row => <RecommendationListEntry row={row} />)}
     </div>);
 };
--- client/app/recommendationListEntry.jsx
+++ client/app/recommendationListEntry.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import styles from './recommendationListEntry.css';
+import { divRow, card, divCell, img, ribbon, price, title, description } from './recommendationListEntry.css';
 
 const RecommendationListEntry = ({ row }) => (
-  <div className={styles.divRow}>{row.map(item => (
-    <div className={styles.card}>
-      <div className={styles.divCell}>
-        <img className={styles.img} src={item.photo_url} alt="Recommended Places" />
-        <div className={styles.ribbon}>
-          <div className={styles.price}>{`from $${item.price} *`}</div>
+  <div className={divRow}>{row.map(item => (
+    <div className={card}>
+      <div className={divCell}>
+        <img className={img} src={item.photo_url} alt="Recommended Places" />
+        <div className={ribbon}>
+          <div className={price}>{`from $${item.price} *`}</div>
         </div>
-        <div className={styles.title}>{item.title}</div>
-        <div className={styles.description}>{item.description}</div><a href="google.com">... read more</a>
+        <div className={title}>{item.title}</div>
+        <div className={description}>{item.description}</div><a href="google.com">... read more</a>
       </div>
     </div>))}
   </div>
