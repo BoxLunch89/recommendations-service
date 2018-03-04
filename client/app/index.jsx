0c1939e8a3aa44ce4164517e599f0d9644df8c04
Zachary <zinouye@gmail.com>
2018-03-03 17:55:53 -0800

    refactored dummy data with new Array(12.fill(*);

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -8,20 +8,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      recs: [
-        {title: "a", description: "a", price: "a", photo_url: "http://lorempixel.com/640/480", _id: "a"},
-        {title: "a", description: "a", price: "a", photo_url: "http://lorempixel.com/640/480", _id: "a"},
-        {title: "a", description: "a", price: "a", photo_url: "http://lorempixel.com/640/480", _id: "a"},
-        {title: "a", description: "a", price: "a", photo_url: "http://lorempixel.com/640/480", _id: "a"},
-        {title: "a", description: "a", price: "a", photo_url: "http://lorempixel.com/640/480", _id: "a"},
-        {title: "a", description: "a", price: "a", photo_url: "http://lorempixel.com/640/480", _id: "a"},
-        {title: "a", description: "a", price: "a", photo_url: "http://lorempixel.com/640/480", _id: "a"},
-        {title: "a", description: "a", price: "a", photo_url: "http://lorempixel.com/640/480", _id: "a"},
-        {title: "a", description: "a", price: "a", photo_url: "http://lorempixel.com/640/480", _id: "a"},
-        {title: "a", description: "a", price: "a", photo_url: "http://lorempixel.com/640/480", _id: "a"},
-        {title: "a", description: "a", price: "a", photo_url: "http://lorempixel.com/640/480", _id: "a"},
-        {title: "a", description: "a", price: "a", photo_url: "http://lorempixel.com/640/480", _id: "a"},
-      ],
+      recs: new Array(4).fill({title: "a", description: "a", price: "a", photo_url: "http://lorempixel.com/640/480", _id: "a"}),
     };
   }
   componentDidMount() {
