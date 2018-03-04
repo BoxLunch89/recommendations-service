cc1a40ef96f2e98f2f0842304be78d02707b41f0
Zachary <zinouye@gmail.com>
2018-03-03 18:01:34 -0800

    removed empty file and empty lines

--- client/app/recommendationListEntry.jsx
+++ client/app/recommendationListEntry.jsx
@@ -1,22 +1,19 @@
 import React from 'react';
 import styles from './recommendationListEntry.css';
 
-const RecommendationListEntry = ({ row }) => {
-
-  return (
-    <div className={styles.divRow}>{row.map(item => (
-      <div className={styles.card}>
-        <div className={styles.divCell}>
-          <img className={styles.img} src={item.photo_url} alt="Recommended Places" />
-          <div className={styles.ribbon}>
-            <div className={styles.price}>{`from $${item.price} *`}</div>
-          </div>
-          <div className={styles.title}>{item.title}</div>
-          <div className={styles.description}>{item.description}</div><a href="google.com">... read more</a>
-          
+const RecommendationListEntry = ({ row }) => (
+  <div className={styles.divRow}>{row.map(item => (
+    <div className={styles.card}>
+      <div className={styles.divCell}>
+        <img className={styles.img} src={item.photo_url} alt="Recommended Places" />
+        <div className={styles.ribbon}>
+          <div className={styles.price}>{`from $${item.price} *`}</div>
         </div>
-      </div>))}
-    </div>);
-};
+        <div className={styles.title}>{item.title}</div>
+        <div className={styles.description}>{item.description}</div><a href="google.com">... read more</a>
+      </div>
+    </div>))}
+  </div>
+);
 
 exports.RecommendationListEntry = RecommendationListEntry;
