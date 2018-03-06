07a1ee7b636d30ea58508a04212cf7e0b5d36cf9
Zachary <zinouye@gmail.com>
2018-03-05 19:42:25 -0800

    made changes to be compatible with proxy server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require('path');
 const { router } = require('./routes');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use('/listings/:listing_id', express.static(path.join(__dirname, '/../client/public')));
+app.use('/', express.static(path.join(__dirname, '/../client/public')));
 app.use('/listings', router);
 
 app.listen(3005, () => {
